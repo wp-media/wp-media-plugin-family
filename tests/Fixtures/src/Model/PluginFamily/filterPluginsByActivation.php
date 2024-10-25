1b827eed34842de10fa3c94fec3a0f32b14be8bd
Michael Lee <38788055+jeawhanlee@users.noreply.github.com>
2024-10-25 17:27:40 +0100

    Release for v1.0.2 (#10)
    * Initial commit
    
    * Added phpcs
    
    * Added lint config
    
    * Updated config file
    
    * Added gitignore
    
    * Fixed phpcs errors
    
    * Ignore composer.lock
    
    * Added tests
    
    * Added patchwork config
    
    * Updated composer packages and script
    
    * Updated method
    
    * Updated namespace
    
    * Updated namespace and methods
    
    * Added test workflow
    
    * Remove test cache
    
    * Ignore test cache
    
    * Updated README
    
    * Updated README
    
    * Updated README
    
    * Updated README
    
    * Updated test
    
    * Updated README
    
    * Updated namespace
    
    * Added phpstan
    
    * Fixed phpstan errors
    
    * Updated composer.json
    
    * Added phpstan config
    
    * Added workflow file for phpstan
    
    * Updated namespace
    
    * use defined var
    
    * Updated README
    
    * Updated tests
    
    * Make wp rocket referrer dynamic
    
    * Updated data
    
    * Updated tests
    
    * Added git attribute file
    
    * Fixed phpcs
    
    * :fixes: #6 change imagify plugin path
    
    * Fix install event for imagify
    
    * Fix imagify urls
    
    * Fix lint error
    
    * fix test error
    
    * Directly reference plugin array copy
    
    * Updated test
    
    * Keep optimize category at the top even when containing main plugin
    
    * Added test
    
    * phpcs fix
    
    * Updated README.md
    
    * Updated tests
    
    ---------
    
    Co-authored-by: Opeyemi Ibrahim <opeyemi.khadri@gmail.com>

--- tests/Fixtures/src/Model/PluginFamily/filterPluginsByActivation.php
+++ tests/Fixtures/src/Model/PluginFamily/filterPluginsByActivation.php
@@ -13,7 +13,7 @@ $plugins = [
 				'desc'  => 'WP Rocket is the easiest way to make your WordPress website faster and boost your Google PageSpeed score. Get more traffic, better engagement, and higher conversions effortlessly.',
 				'link'  => 'https://wp-rocket.me/?utm_source=imagify-coupon&utm_medium=plugin&utm_campaign=imagify',
 			],
-			'imagify-plugin/imagify' => [
+			'imagify/imagify' => [
 				'logo'  => [
 					'file'  => 'logo-imagify.svg',
 					'width' => '50%',
@@ -116,7 +116,7 @@ $expectedActivePluginAsTheLastElement = [
     'optimize_performance' => [
 		'title'   => 'Optimize Performance',
 		'plugins' => [
-			'imagify-plugin/imagify' => [
+			'imagify/imagify' => [
 				'logo'  => [
 					'file'  => 'logo-imagify.svg',
 					'width' => '50%',
@@ -202,7 +202,7 @@ $expectedActivateTextIfPluginIsAlreadyInstalled = [
             'url'  => 'http://example.org/wp-admin/admin-post.php?action=plugin_family_install_wp-rocket&_wpnonce=9a68f00b8d&plugin_to_install=wp-rocket%2Fwp-rocket'
         ],
     ],
-    'imagify-plugin/imagify' => [
+    'imagify/imagify' => [
         'logo'  => [
             'file'  => 'logo-imagify.svg',
             'width' => '50%',
@@ -257,7 +257,7 @@ $expectedUniqueInstallLinkForImagify = [
             'url'  => 'https://wp-rocket.me/?utm_source=-coupon&utm_medium=plugin&utm_campaign='
         ],
     ],
-    'imagify-plugin/imagify' => [
+    'imagify/imagify' => [
         'logo'  => [
             'file'  => 'logo-imagify.svg',
             'width' => '50%',
@@ -298,6 +298,146 @@ $expectedUniqueInstallLinkForImagify = [
     ],
 ];
 
+$expectedWithSecondOrLastPluginActiveInCategory = [
+    'optimize_performance' => [
+		'title'   => 'Optimize Performance',
+		'plugins' => [
+			'imagify/imagify' => [
+				'logo'  => [
+					'file'  => 'logo-imagify.svg',
+					'width' => '50%',
+				],
+				'title' => 'Speed Up Your Website With Lighter Images',
+				'desc'  => 'Imagify is the easiest WordPress image optimizer. It automatically compresses images, converts them to WebP and AVIF formats, and lets you resize and optimize with just one click!',
+				'link'  => 'https://imagify.io/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=install_imagify_from_partner_wp-rocket&_wpnonce=9a68f00b8d&_wp_http_referer=https%3A%2F%2Fexample.org%2Fwp-admin'
+                ],
+			],
+		],
+	],
+    'boost_traffic'        => [
+		'title'   => 'Boost Traffic',
+		'plugins' => [
+			'seo-by-rank-math/rank-math' => [
+				'logo'  => [
+					'file'  => 'logo-rank-math.svg',
+					'width' => '60%',
+				],
+				'title' => 'The Swiss Army Knife of SEO Tools',
+				'desc'  => 'Rank Math SEO is the Best WordPress SEO plugin with the features of many SEO and AI SEO tools in a single package to help multiply your SEO traffic.',
+				'link'  => 'https://rankmath.com/wordpress/plugin/seo-suite/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=plugin_family_install_seo-by-rank-math&_wpnonce=9a68f00b8d&plugin_to_install=seo-by-rank-math%2Frank-math'
+                ], 
+			],
+		],
+	],
+	'protect_secure'       => [
+		'title'   => 'Protect & Secure',
+		'plugins' => [
+			'backwpup/backwpup'                   => [
+				'logo'  => [
+					'file'  => 'logo-backwpup.svg',
+					'width' => '60%',
+				],
+				'title' => 'The Easiest Way to Protect Your Website',
+				'desc'  => 'BackWPup is the most comprehensive and user-friendly backup & restore plugin for WordPress. Easily schedule automatic backups, securely store and restore with just a few clicks!',
+				'link'  => 'https://backwpup.com/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=plugin_family_install_backwpup&_wpnonce=9a68f00b8d&plugin_to_install=backwpup%2Fbackwpup'
+                ],
+			],
+			'uk-cookie-consent/uk-cookie-consent' => [
+				'logo'  => [
+					'file'  => 'logo-termly.svg',
+					'width' => '50%',
+				],
+				'title' => 'GDPR/CCPA Cookie Consent Banner',
+				'desc'  => 'One of the easiest, most comprehensive, and popular cookie consent plugins available. Google Gold Certified Partner to quickly comply with data privacy laws from around the world.',
+				'link'  => 'https://termly.io/resources/articles/wordpress-cookies-guide/',
+                'cta'   => [
+                    'text' => 'Activated',
+                    'url'  => '#'
+                ],
+			],
+		],
+	],
+];
+
+$expectedOptimizeCategoryAsFirstWhenContainingMainPlugin = [
+    'optimize_performance' => [
+		'title'   => 'Optimize Performance',
+		'plugins' => [
+			'imagify/imagify' => [
+				'logo'  => [
+					'file'  => 'logo-imagify.svg',
+					'width' => '50%',
+				],
+				'title' => 'Speed Up Your Website With Lighter Images',
+				'desc'  => 'Imagify is the easiest WordPress image optimizer. It automatically compresses images, converts them to WebP and AVIF formats, and lets you resize and optimize with just one click!',
+				'link'  => 'https://imagify.io/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=install_imagify_from_partner_wp-rocket&_wpnonce=9a68f00b8d&_wp_http_referer=https%3A%2F%2Fexample.org%2Fwp-admin'
+                ],
+			],
+		],
+	],
+    'boost_traffic'        => [
+		'title'   => 'Boost Traffic',
+		'plugins' => [
+			'seo-by-rank-math/rank-math' => [
+				'logo'  => [
+					'file'  => 'logo-rank-math.svg',
+					'width' => '60%',
+				],
+				'title' => 'The Swiss Army Knife of SEO Tools',
+				'desc'  => 'Rank Math SEO is the Best WordPress SEO plugin with the features of many SEO and AI SEO tools in a single package to help multiply your SEO traffic.',
+				'link'  => 'https://rankmath.com/wordpress/plugin/seo-suite/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=plugin_family_install_seo-by-rank-math&_wpnonce=9a68f00b8d&plugin_to_install=seo-by-rank-math%2Frank-math'
+                ], 
+			],
+		],
+	],
+	'protect_secure'       => [
+		'title'   => 'Protect & Secure',
+		'plugins' => [
+			'backwpup/backwpup'                   => [
+				'logo'  => [
+					'file'  => 'logo-backwpup.svg',
+					'width' => '60%',
+				],
+				'title' => 'The Easiest Way to Protect Your Website',
+				'desc'  => 'BackWPup is the most comprehensive and user-friendly backup & restore plugin for WordPress. Easily schedule automatic backups, securely store and restore with just a few clicks!',
+				'link'  => 'https://backwpup.com/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=plugin_family_install_backwpup&_wpnonce=9a68f00b8d&plugin_to_install=backwpup%2Fbackwpup'
+                ],
+			],
+			'uk-cookie-consent/uk-cookie-consent' => [
+				'logo'  => [
+					'file'  => 'logo-termly.svg',
+					'width' => '50%',
+				],
+				'title' => 'GDPR/CCPA Cookie Consent Banner',
+				'desc'  => 'One of the easiest, most comprehensive, and popular cookie consent plugins available. Google Gold Certified Partner to quickly comply with data privacy laws from around the world.',
+				'link'  => 'https://termly.io/resources/articles/wordpress-cookies-guide/',
+                'cta'   => [
+                    'text' => 'Install',
+                    'url'  => 'http://example.org/wp-admin/admin-post.php?action=plugin_family_install_uk-cookie-consent&_wpnonce=9a68f00b8d&plugin_to_install=uk-cookie-consent%2Fuk-cookie-consent'
+                ],
+			],
+		],
+	],
+];
+
 return [
     'testShouldReturnEmptyArrayIfEmptyArrayParsed' => [
         'config' => [
@@ -312,14 +452,14 @@ return [
             'plugins' => $plugins,
             'main_plugin' => 'wp-rocket/wp-rocket',
             'order' => 'categorized',
-            'active_plugin' => 'imagify-plugin/imagify.php',
+            'active_plugin' => 'imagify/imagify.php',
         ],
         'expected' => $expectedActivePluginAsTheLastElement,
     ],
     'testShouldReturnCategoryWithActivePluginAsTheLastElement' => [
         'config' => [
             'plugins' => $plugins,
-            'main_plugin' => 'imagify-plugin/imagify',
+            'main_plugin' => 'imagify/imagify',
             'order' => 'uncategorized',
             'active_plugin' => 'seo-by-rank-math/rank-math.php',
         ],
@@ -344,4 +484,22 @@ return [
         ],
         'expected' => $expectedUniqueInstallLinkForImagify,
     ],
+    'testShouldReturnAsExpectedWithSecondOrLastPluginActiveInCategory' => [
+        'config' => [
+            'plugins' => $plugins,
+            'main_plugin' => 'wp-rocket/wp-rocket',
+            'order' => 'categorized',
+            'active_plugin' => 'uk-cookie-consent/uk-cookie-consent.php',
+        ],
+        'expected' => $expectedWithSecondOrLastPluginActiveInCategory,
+    ],
+    'testShouldStillReturnOptimizeCategoryAsFirstWhenContainingMainPlugin' => [
+        'config' => [
+            'plugins' => $plugins,
+            'main_plugin' => 'wp-rocket/wp-rocket',
+            'order' => 'categorized',
+            'active_plugin' => 'wp-rocket/wp-rocket.php',
+        ],
+        'expected' => $expectedOptimizeCategoryAsFirstWhenContainingMainPlugin,
+    ]
 ];
